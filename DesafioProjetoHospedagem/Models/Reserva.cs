cbfc4aae519e780085935889d59fdeb88fb659c1
Keoma Kevin Baudin <keoma4@gmail.com>
2023-09-23 03:12:34 -0300

    refactor: refatora Reserva

--- DesafioProjetoHospedagem/Models/Reserva.cs
+++ DesafioProjetoHospedagem/Models/Reserva.cs
@@ -2,50 +2,39 @@ namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
     {
+        private const int QuantidadeDiasParaObterDesconto = 10;
+        private const decimal PorcentagemDesconto = 0.1M;
+
         public List<Pessoa> Hospedes { get; set; }
         public Suite Suite { get; set; }
         public int DiasReservados { get; set; }
 
         public Reserva() { }
 
-        public Reserva(int diasReservados)
-        {
-            DiasReservados = diasReservados;
-        }
+        public Reserva(int diasReservados) => DiasReservados = diasReservados;
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            if (Suite.Capacidade >= hospedes.Count)
-            {
-                Hospedes = hospedes;
-            }
-            else
-            {
+            if (Suite.Capacidade < hospedes.Count)
                 throw new ArgumentException("A quantidade de hóspedes não pode exceder a capacidade da suíte");
-            }
+
+            Hospedes = hospedes;
         }
 
-        public void CadastrarSuite(Suite suite)
-        {
-            Suite = suite;
-        }
+        public void CadastrarSuite(Suite suite) => Suite = suite;
 
-        public int ObterQuantidadeHospedes()
-        {
-            return Hospedes.Count;
-        }
+        public int ObterQuantidadeHospedes() => Hospedes.Count;
 
         public decimal CalcularValorDiaria()
         {
             decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            if (DiasReservados >= 10)
-            {
-                valor *= 0.9M;
-            }
+            if (ElegivelDesconto())
+                valor *= 1 - PorcentagemDesconto;
 
             return valor;
         }
+
+        private bool ElegivelDesconto() => DiasReservados >= QuantidadeDiasParaObterDesconto;
     }
 }
